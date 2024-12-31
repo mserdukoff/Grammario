3fcf2435c6df20bbd8f177400e0577ba7891c5bb
Matt Serdukoff <m.serdukoff@gmail.com>
2024-12-31 14:29:47 -0500

    v0.1.0

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="fixed bottom-0 left-0 right-0 z-10 p-4">
       <p className="text-sm text-gray-600 text-center">
-        This is still being developed and is filled with bugs. <br />If you are seeing this, it is because I have sent it to you and want feedback. Thank you!
+        Grammario v0.1.0<br />This is still being developed and is filled with bugs. <br />If you are seeing this, it is because I have sent it to you and want feedback. Thank you!
       </p>
     </footer>
   )
