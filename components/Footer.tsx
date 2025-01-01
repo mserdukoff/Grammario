cec6523ea6c64ffd987356ad755c5980b5ba361d
Matt Serdukoff <m.serdukoff@gmail.com>
2025-01-01 12:23:16 -0500

    added link to mattserdukoff

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,9 @@ const Footer: React.FC = () => {
   return (
     <footer className="fixed bottom-0 left-0 right-0 z-10 p-4">
       <p className="text-sm text-gray-600 text-center">
-        Grammario v0.1.0<br />This is still being developed and is filled with bugs. <br />If you are seeing this, it is because I have sent it to you and want feedback. Thank you!
+        This is still being developed and is filled with bugs. If you are seeing this, it is because I have sent it to you and want feedback. Thank you!
+        <br />
+        Grammario by <a href="https://www.mattserdukoff.com" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">Matt Serdukoff</a>
       </p>
     </footer>
   )
