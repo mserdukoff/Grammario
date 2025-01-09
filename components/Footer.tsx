54d05aaa521a67c0f8113e649e2721f822c01b83
Matt Serdukoff <m.serdukoff@gmail.com>
2025-01-09 13:00:55 -0500

    removed double footer and fixing sidebar again

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,6 @@ const Footer: React.FC = () => {
     <footer className="w-full bottom-0 left-0 right-0 z-10 p-4 bg-background border-t border-border">
       <p className="text-sm text-gray-600 text-center">
         This is still being developed and is filled with bugs. If you are seeing this, it is because I have sent it to you and want feedback. Thank you!
-        <br />
-        Grammario by <a href="https://www.mattserdukoff.com" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">Matt Serdukoff</a>
       </p>
     </footer>
   )
